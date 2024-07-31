b20ceab870b98f80cf8035d7d754612676f73bbd
techupdate712 <techupdate712@gmail.com>
2024-07-31 10:57:35 +0530

    Update 0009-palindrome-number.py

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -10,3 +10,22 @@ class Solution:
             else:
                 return False
         return True
+        
+#Easy Solution
+# def isPalindrome(self, x: int) -> bool:
+# 	if x < 0:
+# 		return False
+	
+# 	return str(x) == str(x)[::-1]
+
+#If we don't want to convert the number to string, then recreate a new number in reverse order.
+# def isPalindrome(self, x: int) -> bool:
+# 	if x<0:
+# 		return False
+
+# 	inputNum = x
+# 	newNum = 0
+# 	while x>0:
+# 		newNum = newNum * 10 + x%10
+# 		x = x//10
+# 	return newNum == inputNum
